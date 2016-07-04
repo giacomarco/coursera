afab13e47edb47b6df72382ee78420a9f65f77f5
giacomarco <marco.giacobazzi85@gmail.com>
2016-07-04 21:47:29 +0200

    Add files via upload

---
+++ mod4_solution/script.js
@@ -0,0 +1,26 @@
+// Module 4 Assignment Instructions. -- Marco Giacobazzi --
+
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+
+*/
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
